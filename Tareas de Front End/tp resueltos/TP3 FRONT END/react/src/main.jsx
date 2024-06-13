0af1efc77aa27bc40d7cd8794178c889e22fe93b
Yonathan Guanco <yonathanguanco@gmail.com>
2024-06-12 23:59:51 -0300

    cambio en el main.jsx

--- Tareas de Front End/tp resueltos/TP3 FRONT END/react/src/main.jsx
+++ Tareas de Front End/tp resueltos/TP3 FRONT END/react/src/main.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Contador from './components2/Contador.jsx'
-import ColorChanger from './components3/Colores.jsx'
-import './components3/Colores.jsx'
-import GeneradorEmoji from './components4/Emojis.jsx'
-import JuegoNumeroAleatorio from './components5/Adivinanza.jsx'
-
+//import App from './App.jsx'
+//import './index.css'
+//import Contador from './components2/Contador.jsx'
+//import ColorChanger from './components3/Colores.jsx'
+//import './components3/Colores.jsx'
+//import GeneradorEmoji from './components4/Emojis.jsx'
+//import JuegoNumeroAleatorio from './components5/Adivinanza.jsx'
+import ComponenteApi from './API/ApiReal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Contador />
-    <ColorChanger />
-    <GeneradorEmoji />
-    < JuegoNumeroAleatorio />
+   
+    
+    <ComponenteApi />
     </React.StrictMode>,
 )
